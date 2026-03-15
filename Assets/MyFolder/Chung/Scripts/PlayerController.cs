e82be1485f495f8438ef261cb221699197505c2f
hireachy1660 <klico1911@gmail.com>
2026-03-15 23:35:22 +0900

    옵스타클 코드 분기 점 추가

--- Assets/MyFolder/Chung/Scripts/PlayerController.cs
+++ Assets/MyFolder/Chung/Scripts/PlayerController.cs
@@ -393,6 +393,7 @@ public class PlayerController : MonoBehaviourPun, IAttackReceiver
         else
         {
             myKnife.Attack(_isHeld);
+            OnAttackEvent?.Invoke();
         }
 
     }
